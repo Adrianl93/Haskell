fabb139f4dcbbcfff23d9c716fbb2ef48d2f833c
Adrianl93 <laborde.adrian2012@gmail.com>
2023-09-23 17:34:00 -0300

    Ejercicio 7

--- Ejercicios/Practico3.hs
+++ Ejercicios/Practico3.hs
@@ -1,2 +1,15 @@
+-- Ejercicio 2
 promedio :: Float -> Float -> Float
 promedio x y = (x + y)/2
+
+
+-- Ejercicio 7
+
+signo :: Int -> Int
+signo x  | x > 0 = 1
+         | x<0 = -1
+         | x==0 = 0
+          
+
+-- Ejercicio 7 A
+
