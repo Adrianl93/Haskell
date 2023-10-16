12b8a2a1ee878ee612fc58e71b3bb393ef7105dd
Adrianl93 <laborde.adrian2012@gmail.com>
2023-10-16 11:51:26 -0300

    ejercicio 7

--- Ejercicios/Practico4.hs
+++ Ejercicios/Practico4.hs
@@ -7,6 +7,9 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldr" #-}
 {-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Use odd" #-}
+{-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Use even" #-}
 
 
 
@@ -70,3 +73,31 @@ multiplica b [] = []
 multiplica b (x:xs) = (b * x) : multiplica b xs
 
 
+
+-- Ejercicio 7: Una función filter es aquella que dada una lista devuelve otra lista cuyos elementos son los elementos de la primera que cumplan una determinada condición, en el mismo orden y con las mismas repeticiones (si las hubiere). Por ejemplo: soloPares : [Int] → [Int] devuelve aquellos elementos de la lista que son pares.
+-- Definí recursivamente las siguientes funciones filter.
+
+
+-- a) soloPares: [Int] → [Int], que dada una lista de enteros xs devuelve una lista sólo con los números pares contenidos en xs, en el mismo orden y con las mismas repeticiones (si las hubiera).
+-- Por ejemplo: soloPares.[3, 0, −2, 12] = [0, −2, 12]
+
+soloPares :: [Int] -> [Int ]
+soloPares [] = []
+soloPares (x:xs) | ( mod x 2 == 0) = x : soloPares xs
+                 | (mod x 2 /= 0) = soloPares xs
+
+
+-- b) mayoresQue10: [Int] → [Int], que dada una lista de enteros xs devuelve una lista sólo con los números mayores que 10 contenidos en xs,
+-- Por ejemplo: mayoresQue10.[3, 0, −2, 12] = [12]
+mayoresQue10 :: [Int] -> [Int]
+mayoresQue10 [] = []
+mayoresQue10 (x:xs) | x > 10 = x : mayoresQue10 xs
+                    | x <= 10 = mayoresQue10 xs
+
+-- c) mayoresQue: Int → [Int] → [Int], que dado un entero n y una lista de enteros xs devuelve una lista sólo con los números mayores que n contenidos en xs,
+-- Por ejemplo: mayoresQue.2.[3, 0, −2, 12] = [3, 12]
+
+mayoresQue :: Int -> [Int] -> [Int]
+mayoresQue b [] = []
+mayoresQue b (x:xs) | x > b = x : mayoresQue b xs
+                    | x <= b = mayoresQue b xs
