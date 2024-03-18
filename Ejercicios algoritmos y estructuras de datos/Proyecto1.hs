ba87c241da406d7e7c05dd694de67906ff629ed2
Adrianl93 <laborde.adrian2012@gmail.com>
2024-03-18 13:24:46 -0300

    ejercicio 4 y 5

--- Ejercicios algoritmos y estructuras de datos/Proyecto1.hs
+++ Ejercicios algoritmos y estructuras de datos/Proyecto1.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE NPlusKPatterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 
 
 -- ejercicio 1
@@ -37,6 +39,12 @@ valorAbsoluto x | x > 0 = x
 -- ejercicio 2
 
 --a)
+paraTodo :: [Bool] -> Bool
+paraTodo [] = True
+paraTodo (x : xs) = x && paraTodo xs
+
+--paraTodo [True, True, True] --> True
+--paraTodo [True, False, True] --> False
 
 
 --b)
@@ -87,3 +95,41 @@ pertenece y (x : xs) = x == y || pertenece y xs
 -- ejercicio 4
 
 --a) 
+paraTodo' :: [a] -> (a -> Bool) -> Bool
+paraTodo' [] t = True
+paraTodo' (x : xs) t = t x && paraTodo' xs t
+
+--paraTodo' [2, 3, 4, 5] (> 0) -> True
+--paraTodo' [1, 4, 5, 7] (==0) -> False
+
+--b)
+existe' :: [a] -> (a -> Bool) -> Bool
+existe' [] t = False
+existe' (x : xs) t = t x || existe' xs t
+
+
+--existe' [1,2,3] (==2) -> True
+--existe' [(-2),2,3] (< 0) -> True
+
+--c) 
+sumatoria' :: [a] -> (a -> Int) -> Int
+sumatoria' [] t = 0
+sumatoria' (x : xs) t = t x + sumatoria' xs t
+
+--sumatoria' [2, 3, 4] (*2) --> 18
+--sumatoria' [4, 8 ,7] (+1) --> 22
+
+--d)
+productoria' :: [a] -> (a -> Int) -> Int
+productoria' [] t = 1
+productoria' (x : xs) t = t x * productoria' xs t
+
+--productoria' [1,2,3] (*2) -> 48
+--productoria' [1,2,3] (+1) -> 24
+
+
+
+-- ejercicio 5
+
+paraTodo'' :: [a] -> (a -> Bool) -> Bool
+paraTodo'' (x:xs) t = t x && paraTodo'' xs t
