d22c8c6f0b15c44f9dfd5b0348545f3908b477c4
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-03-22 10:20:41 -0300

    ejercicio 11

--- Ejercicios algoritmos y estructuras de datos/Proyecto1.hs
+++ Ejercicios algoritmos y estructuras de datos/Proyecto1.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE NPlusKPatterns #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+import System.Win32 (COORD(xPos))
 {-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Redundant bracket" #-}
 
 
 -- ejercicio 1
@@ -199,8 +203,16 @@ factorial' n = productoria' [1..n] (*1)
 
 --g)
 
+filtro :: [a] -> (a -> Bool) ->[a]
+filtro [] f = []
+filtro (x : xs) f | f x  = x : filtro xs f
+                  | not (f x)  = filtro xs f 
+
 multiplicaPrimos :: [Int] -> Int 
-multiplicaPrimos xs = productoria' (filter esPrimo xs) (*1)
+multiplicaPrimos xs = productoria' (filtro xs esPrimo) (*1)
+
+
+                  
 
 --multiplicaPrimos [2, 3, 5, 8] --> 30
 --multiplicaPrimos [2,3,8,12] --> 6
@@ -232,4 +244,128 @@ todosFib :: [Int] -> Bool
 todosFib xs = paraTodo' xs esFib  
 
 --todosFib [34, 55, 89] --> True
---todosFib [34, 55, 88] --> True
+--todosFib [34, 55, 88] --> False
+
+
+--Ejercicio 7
+
+--Indagá en Hoogle sobre las funciones map y filter. También podes consultar su tipo en ghci con el comando :t.
+
+--a) ¿Qué hacen estas funciones?
+
+--La función Map toma una lista de elementos de tipo a y una funcion cualquiera y aplica dicha funcion sobre todos los elementos de la lista y los vuelve a unir para formar una lista.
+
+--La función Filter toma una lista de elementos y una funcion que devuelve un booleano,luego aplica esta funcion a cada elemento de la lista y conserva unicamente los elementos que devuelven True al aplicar la función 
+
+
+-- b) ¿A qué equivale la expresión map succ [1, -4, 6, 2, -8], donde succ n = n+1?
+-- La expresion aplica map a una lista de numeros y la funcion succ, que devuelve el numero siguiente a n, de esta manera la expresion devuelve una lista con el numero siguiente a cada uno de los numeros enviados, en este caso : [2, 3, 7, 3, 7]
+
+--c)¿Y la expresión filter esPositivo [1, -4, 6, 2, -8]?
+
+--La expresión utiliza filter en una lista de numeros junto con la función esPositivo, por lo cual deberia devolver una lista donde solo queden elementos que satisfacen la propiedad de ser números iguales o mayores a 0, en este caso : [1, 6, 2]
+
+
+--Ejercicio 8. 
+
+--a) Definila usando recursión.
+duplica :: [Int] -> [Int]
+duplica [] = []
+duplica (x: xs) = x * 2 : duplica xs
+
+
+--duplica [2, 4 , 6] --> [4, 8, 12]
+--duplica [5, 10, 11] --> [10, 20, 22]
+
+--b) Definila utilizando la función map.
+duplica' :: [Int] -> [Int]
+duplica' xs = map (*2) xs
+
+
+--duplica' [6, 7, 9] --> [12, 14, 18]
+--duplica' [9, 11, 33] --> [18, 28 , 32]
+
+--Programá una función que dada una lista de números xs, calcula una lista que tiene como
+--elementos aquellos números de xs que son primos.
+
+--a) Definila usando recursión.
+soloPrimos :: [Int] -> [Int]
+soloPrimos [] = []
+soloPrimos (x : xs) | esPrimo x  = x : soloPrimos xs
+                    |otherwise = soloPrimos xs  
+
+--soloPrimos [2,5,7,9,11,18] --> [2, 5, 7, 11]
+--soloPrimos [2,5,7,9,11,1,17,20,22] --> [2, 5, 7, 11, 1, 17]
+
+--b) Definila utilizando la función filter.
+
+soloPrimos' :: [Int] -> [Int]
+soloPrimos' xs = filter esPrimo xs
+
+--soloPrimos' [2,5,7,9,11,18] --> [2, 5, 7, 11]
+--soloPrimos' [2,5,7,9,11,1,17,20,22] --> [2, 5, 7, 11, 1, 17]
+
+--c) Revisá tu definición del ejercicio 6g. ¿Se puede mejorar?
+
+
+
+-- Anteriormente habia creado una funcion auxiliar que realizaba el mismo trabajo que Filter 
+multiplicaPrimos' :: [Int] -> Int
+multiplicaPrimos' xs = productoria' (filter esPrimo xs) (*1)
+
+
+--multiplicaPrimos' [2,5,6,7] --> 70
+--multiplicaPrimos' [2,5,6,7,11,4] --> 770
+
+--Ejercicio 10
+
+
+
+--a) Programá primIgualesA por recursión.
+
+
+primIgualesA :: Eq a => a -> [a] -> [a]
+primIgualesA x [] = []
+primIgualesA x (y : z : ys) | (x == y && x == z )= y : z : primIgualesA x ys
+                        | otherwise = []
+
+--primIgualesA 1 [1, 1, 1, 1, 2, 3, 4, 5, 5, 5, 5] --> [1, 1, 1, 1]
+--primIgualesA 1 [1, 2] --> []
+--primIgualesA 'a' "aaabbbjashaks" -->  "aaa"
+
+
+--b)  Programá nuevamente la función utilizando takeWhile
+
+primIgualesA' :: Eq a => a -> [a] -> [a]
+primIgualesA' x = takeWhile (== x)  
+
+--primIgualesA' 2 [2, 2, 2, 3, 4, 5, 5, 5, 5, 5] --> [2, 2, 2]
+--primIgualesA' 'f' "fff000" --> "fff"
+
+-- Ejercicio 11
+
+--a) Program´a primIguales por recursion
+primIguales ::Eq a => [a] -> [a]
+primIguales [] = []
+primIguales [x] = [x]
+primIguales (x : y : ys) | x == y = x : primIguales (y : ys)
+                         | x /= y = [x]
+
+
+-- primIguales [1, 2, 3] --> [1]
+-- primIguales [1, 1, 1, 2, 3] --> [1, 1, 1]
+-- primIguales "aaaaalo" --> "aaaa"
+
+--b)
+
+
+-- b) Usa cualquier version de primIgualesA para programar primIguales. Esta permitido
+-- dividir en casos, pero no usar recursion.
+
+
+primIguales' :: Eq a => [a] -> [a]
+primIguales' (x:xs) = primIgualesA' x xs
+
+--primIguales [2, 3, 4, 5] --> [2]
+--primIguales [2, 2, 2, 4, 5, 5, 5] --> [2, 2, 2]
+--primIguales "aaaabbb" --> "aaaa"
