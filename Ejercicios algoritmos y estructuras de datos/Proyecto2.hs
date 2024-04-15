9a220af80265c55373f56ebf7c8210a5811aa23c
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-04-15 11:55:33 -0300

    ejercicio 4

--- Ejercicios algoritmos y estructuras de datos/Proyecto2.hs
+++ Ejercicios algoritmos y estructuras de datos/Proyecto2.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NPlusKPatterns #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Use camelCase" #-}
 
 
 -- Ejercicio 1 Tipos Enumerados
@@ -99,3 +100,68 @@ instance Bounded NotaBasica where
 
     -- minimoElemento [Fa, La, Sol, Re, Fa]  --> Re
     -- minimoElemento' [Fa, La, Sol, Re, Fa] --> Re
+
+
+-- Ejercicio 4 Sinonimo de tipos; constructores con parametros. 
+
+
+--a)
+-- Implementa el tipo Deportista y todos sus tipos accesorios (NumCamiseta, Altura,Zona, etc) tal como estan definidos arriba.
+
+-- lista de deportistas para testeo
+deportistas :: [Deportista]
+deportistas = [Velocista 170, Ciclista Pista, Tenista DosManos Izquierda 180, Futbolista Delantera 10 Derecha 175, Velocista 160, Futbolista Arco 1 Izquierda 175]
+
+type Altura = Int
+type NumCamiseta = Int
+
+data Zona = Arco | Defensa | Mediocampo | Delantera deriving (Eq)
+data TipoReves = DosManos | UnaMano deriving (Eq)
+data Modalidad = Carretera | Pista | Monte | BMX deriving (Eq)
+data PiernaHabil = Izquierda | Derecha deriving (Eq)
+
+type ManoHabil = PiernaHabil
+
+data Deportista = Ajedrecista
+                |Ciclista Modalidad
+                |Velocista Altura
+                |Tenista TipoReves ManoHabil Altura
+                |Futbolista Zona NumCamiseta PiernaHabil Altura
+
+
+--b) ¿Cual es el tipo del contructor Ciclista?
+
+--Ciclista :: Modalidad -> Deportista
+
+--El constructor ciclista es del tipo Deportista y necesita un parametro (Modalidad)
+
+--c) Programa la funcion contar_velocistas :: [Deportista] -> Int que dada una lista de deportistas xs, devuelve la cantidad de velocistas que hay dentro de xs
+
+contar_velocistas :: [Deportista] -> Int
+contar_velocistas [] = 0
+contar_velocistas (x : xs) | esVelocista x = 1 + contar_velocistas xs
+                           | otherwise = contar_velocistas xs
+                           where esVelocista (Velocista {}) = True
+                                 esVelocista _ = False
+
+--contar_velocistas deportistas --> 2
+
+--d)  Programa la funcion contar_futbolistas :: [Deportista] -> Zona -> Int que dada una lista de deportistas xs, y una zona z, devuelve la cantidad de futbolistas incluidos en xs que juegan en la zona z. Programa contar_futbolistas sin usar igualdad, utilizando pattern matching.
+
+contar_futbolistas :: [Deportista] -> Zona -> Int
+contar_futbolistas [] _ = 0
+contar_futbolistas ((Futbolista zona _ _ _) : xs) z | zona == z = 1 + contar_futbolistas xs z
+                                                    |otherwise = contar_futbolistas xs z
+contar_futbolistas (x:xs) z = contar_futbolistas xs z
+
+--contar_futbolistas deportistas Arco --> 1
+
+
+--e)¿La funcion anterior usa filter? Si no es ası, reprogramala para usarla
+
+contar_futbolistas' :: [Deportista] -> Zona -> Int
+contar_futbolistas' deportistas z = length (filter (esFutbolistaDeZona z) deportistas)
+    where esFutbolistaDeZona z (Futbolista zona _ _ _) = z == zona
+          esFutbolistaDeZona _ _ = False
+
+--contar_futbolistas' deportistas Arco  --> 1
