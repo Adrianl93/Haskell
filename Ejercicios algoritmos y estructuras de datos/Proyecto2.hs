98ee869fad5dbc02e1f73351fd58717c3b057398
Adrianl93 <[200~laborde.adrian2012@gmail.comgit config --global user.email laborde.adrian2012@gmail.com>
2024-04-16 17:35:32 -0300

    Ejercicio 8 solved

--- Ejercicios algoritmos y estructuras de datos/Proyecto2.hs
+++ Ejercicios algoritmos y estructuras de datos/Proyecto2.hs
@@ -282,3 +282,108 @@ busca (Encolada deportista colaRestante) zona =
 
 -- Cola podria parecerse bastante a una Lista, con la diferencia de que no hay acceso rapido al último elemento de la cola, y su funcionamiento es del tipo First In First Out similar a una cola de supermercado, o en inglés Queue.
 
+
+
+
+--Ejercicio 8 Tipos recursivos y polimórficos
+
+
+data ListaAsoc a b = Vacia | Nodo a b ( ListaAsoc a b ) deriving Show
+type Diccionario = ListaAsoc String String
+type Padron = ListaAsoc Int String
+
+-- Listas aux para testeo
+
+listaEjemplo :: ListaAsoc String Int
+listaEjemplo = Nodo "clave1" 1 (Nodo "clave2" 2 Vacia)
+
+listaEjemplo2 :: ListaAsoc Char Int
+listaEjemplo2 = Nodo 'a' 1 (Nodo 'b' 2 (Nodo 'c' 3 Vacia))
+
+
+
+listaVacia :: ListaAsoc Int String
+listaVacia = Vacia
+
+listaUno :: ListaAsoc Int String
+listaUno = Nodo 1 "dato1" Vacia
+
+listaTres :: ListaAsoc Int String
+listaTres = Nodo 1 "dato1" (Nodo 2 "dato2" (Nodo 3 "dato3" Vacia))
+
+-- a) ¿Como se debe instanciar el tipo ListaAsoc para representar la informacion almacenada en una guıa telefonica?
+
+type GuiaTelefonica = ListaAsoc String String
+
+--b) Programa las siguientes funciones:
+
+--1) la_long :: ListaAsoc a b -> Int que devuelve la cantidad de datos en una lista.
+
+la_long :: ListaAsoc a b -> Int 
+la_long Vacia = 0
+la_long (Nodo _ _ resto) = 1 + la_long resto
+
+
+--la_long listaVacia --> 0
+--la_long listaUno --> 1
+--la_long listaTres --> 3
+
+--2)la_concat :: ListaAsoc a b -> ListaAsoc a b -> ListaAsoc a b, que devuelve la concatenacion de dos listas de asociaciones.
+
+la_concat :: ListaAsoc a b -> ListaAsoc a b -> ListaAsoc a b
+la_concat Vacia lista = lista
+la_concat (Nodo clave valor resto1) lista2 = Nodo clave valor (la_concat resto1 lista2)
+
+--la_concat listaUno listaTres   --> Nodo 1 "dato1" (Nodo 1 "dato1" (Nodo 2 "dato2" (Nodo 3 "dato3" Vacia)))
+--la_concat listaVacia listaTres --> Nodo 1 "dato1" (Nodo 2 "dato2" (Nodo 3 "dato3" Vacia))
+
+--3)la_agregar :: Eq a => ListaAsoc a b -> a -> b -> ListaAsoc a b, que agrega un nodo a la lista de asociaciones si la clave no esta en la lista, o actualiza el valor si la clave ya se encontraba.
+
+
+la_agregar :: Eq a => ListaAsoc a b -> a -> b -> ListaAsoc a b
+la_agregar Vacia nuevaClave nuevoValor = Nodo nuevaClave nuevoValor Vacia
+la_agregar (Nodo clave valor resto) nuevaClave nuevoValor
+    | clave == nuevaClave = Nodo clave nuevoValor resto
+    | otherwise = Nodo clave valor (la_agregar resto nuevaClave nuevoValor)
+
+
+--la_agregar listaEjemplo "clave3" 3  --> Nodo "clave1" 1 (Nodo "clave2" 2 (Nodo "clave3" 3 Vacia))
+--la_agregar listaEjemplo "clave1" 10 --> Nodo "clave1" 10 (Nodo "clave2" 2 Vacia)
+
+
+--4) la_pares :: ListaAsoc a b -> [(a, b)] que transforma una lista de asociaciones en una lista de pares clave-dato.
+
+la_pares :: ListaAsoc a b -> [(a, b)]
+la_pares Vacia = []
+la_pares (Nodo clave valor resto) = (clave, valor) : la_pares resto
+
+
+pares :: [(Char, Int)]
+pares = la_pares listaEjemplo2
+
+--print pares --> [('a',1),('b',2),('c',3)]
+
+
+-- 5)  la_busca :: Eq a => ListaAsoc a b -> a -> Maybe b que dada una lista y una clave devuelve el dato asociado, si es que existe. En caso contrario devuelve Nothing.
+
+la_busca :: Eq a => ListaAsoc a b -> a -> Maybe b
+la_busca Vacia _ = Nothing
+la_busca (Nodo clave valor resto) buscada
+    | clave == buscada = Just valor
+    | otherwise = la_busca resto buscada
+
+    --la_busca listaEjemplo "clave2" --> Just 2
+    --la_busca listaEjemplo "clave4" --> Nothing
+
+
+
+-- 6) la_borrar :: Eq a => a -> ListaAsoc a b -> ListaAsoc a b que dada una clave a elimina la entrada en la lista
+
+la_borrar :: Eq a => a -> ListaAsoc a b -> ListaAsoc a b
+la_borrar _ Vacia = Vacia
+la_borrar a (Nodo clave valor resto)
+    | clave == a = resto
+    | otherwise = Nodo clave valor (la_borrar a resto)
+
+    --listaBorrada = la_borrar "clave2" listaEjemplo
+    -- print listaBorrada --> Nodo "clave1" 1 Vacia
