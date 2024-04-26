6535962bbb6d00ab705f045a10a18ec66f7a8e00
Adrianl93 <laborde.adrian2012@gmail.com>
2024-04-26 11:00:54 -0300

    simulacro de parcial 1

---
+++ Ejercicios algoritmos y estructuras de datos/SimParcial1.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NPlusKPatterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+import System.Win32 (COORD(xPos))
+{-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Redundant bracket" #-}
+
+
